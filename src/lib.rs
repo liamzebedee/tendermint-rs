369a5ab055ae0a8bccb84218b9b03e42bb133d19
liamzebedee <liamzebedee@yahoo.com.au>
2025-06-05 16:54:43 +1000

    mv stuff into old/ for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,11 @@
 pub mod algos;
-pub mod cli;
+// pub mod cli;
 pub mod clock;
 pub mod config;
 pub mod crypto;
 pub mod events;
-pub mod messages;
 pub mod params;
-pub mod process;
 pub mod protos;
 pub mod proposer_choice;
-pub mod rpc_client;
-pub mod rpc_server;
+pub mod types;
 
-#[cfg(test)]
-mod tests {
-    use crypto::verify_signature;
-
-    #[test]
-    fn test_create_sign() {
-        let keypair = crypto::ECDSAKeypair::new();
-        let data = b"gm tendermint";
-
-        let signature = keypair.sign(data);
-        assert!(verify_signature(data, &signature.to_inner(), keypair.get_public_key()));
-
-        println!("Signature verified successfully!");
-    }
-}
